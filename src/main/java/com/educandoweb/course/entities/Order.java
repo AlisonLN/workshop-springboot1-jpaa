55b85ae632a14a34a2e893f4e2cc3d12609e0d56
AlisonLN <alison.nascimento@hotmail.com>
2024-02-27 15:26:05 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,38 +2,30 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
 	private Instant moment;
-	
 	private Integer orderStatus;
-	
 	@ManyToOne
 	@JoinColumn(name = "Client_ID")
 	private User client;
-	
-	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = 	new HashSet<>();
 	public Order() {
 	}
 
@@ -70,20 +62,20 @@ public class Order implements Serializable {
 		this.orderStatus = orderStatus.getCode();
 		}
 	}
-
 	public User getClient() {
 		return client;
 	}
-
 	public void setClient(User client) {
 		this.client = client;
 	}
 
+	public Set<OrderItem> getItems(){
+		return items;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
