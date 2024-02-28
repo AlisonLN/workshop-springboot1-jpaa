dbdc976e3039c98121e7d12df18e13aed67d0717
AlisonLN <alison.nascimento@hotmail.com>
2024-02-28 15:48:41 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,8 +4,11 @@ package com.educandoweb.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.educandoweb.course.resources.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
@@ -28,8 +31,14 @@ public class UserService {
 	public User insert (User obj) {
 		return repository.save(obj);
 	}
-	public void delete(Long id){
-		repository.deleteById(id);
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e){
+			throw  new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e){
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	public User update(Long id, User obj){
 		User entity = repository.getReferenceById(id);
--- src/main/java/com/educandoweb/course/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/educandoweb/course/services/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.educandoweb.course.services.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1l;
+    private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(Object id){
         super("Resource not found.ID" + id);
